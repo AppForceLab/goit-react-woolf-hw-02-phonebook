f0ddb25e5d5db372d50024d005fa8b3aa878059e
VolodymyrPopov <smartappsvp@gmail.com>
2024-02-25 18:23:42 +0000

    dz 2 part 2

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,14 @@
+import ContactListItem from '../ContactListItem/ContactListItem';
+import './ContactList.css';
+
+const ContactList = ({ contacts, onDelete }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <ContactListItem key={contact.id} contact={contact} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,11 @@
+
+const ContactListItem = ({ contact, onDelete }) => {
+  return (
+    <li>
+      {contact.name} - {contact.number}
+      <button onClick={() => onDelete(contact.id)}>Delete</button>
+    </li>
+  );
+};
+
+export default ContactListItem;
